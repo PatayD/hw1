5e3c790723f5d40c0c8e8efe0993cdeac2f5c4d8
PatayDaunbl <podenko.ak@students.dvfu.ru>
2023-04-04 13:13:23 +1000

    hw1

---
+++ hw1.js
@@ -0,0 +1,21 @@
+let chek = 'bread28muka31.23soli250'
+let invalidChek = 'bread28.1muka24.31soli250.232627' 
+
+function Numb(str) {
+    let result = str.match(/(-?\d+(\.\d+)?)/g).map(v => +v);
+    let rez = Object.values(result)
+    for(let i=0;i < rez.length;i++){
+        if((rez[i].toString().match(/\.(\d+)/)?.[1].length) > 2){
+            
+            console.log('invalid chek') 
+            break   
+        }
+        if((rez[i].toString().match(/\.(\d+)/)?.[1].length) <= 2) {
+            console.log(rez.reduce((acc,rez) => (acc + rez), 0))
+        }
+    }
+    // console.log(result)
+
+}
+
+Numb(invalidChek)
